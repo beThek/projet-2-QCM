fbe298d0daaf31d4756d212b85ab2093d855172c
bethek <kreshnikiberdemaj@gmail.com>
2017-06-26 15:28:01 +0200

    php fonctionnel

--- porjet-2bis-QCM/reponses.php
+++ porjet-2bis-QCM/reponses.php
@@ -12,36 +12,38 @@
 
 				<?php
 
+				
+
 					if(isset($_POST["envoyer"])){
-					$email = $_POST["mail"];
-					$name = $_POST["name"];
+						$email = $_POST["mail"];
+						$name = $_POST["name"];
 
 
 
-				// the message
-				$msg = "Message reçu de Quizz from server";
+					// the message
+					$msg = "Message reçu de Quizz from server";
 
 
 
 
-				// send email
-				mail("kreshnikiberdemaj@gmail.com","QCM reponses",$msg);
-				mail("info@bethek.5gbfree.com","QCM reponses",$msg);
+					// send email
+					mail("kreshnikiberdemaj@gmail.com","QCM reponses",$msg);
+					mail("info@bethek.5gbfree.com","QCM reponses",$msg);
 
 
-            $answer1 = $_POST['question-1-answers'];
-            $answer2 = $_POST['question-2-answers'];
-            $answer3 = $_POST['question-3-answers'];
-						$answer4 = $_POST['question-4-answers'];
-						$answer5 = $_POST['question-5-answers'];
+		            $answer1 = $_POST['question-1-answers'];
+		            $answer2 = $_POST['question-2-answers'];
+		            $answer3 = $_POST['question-3-answers'];
+					$answer4 = $_POST['question-4-answers'];
+					$answer5 = $_POST['question-5-answers'];
 
 
 
             $totalCorrect = 0;
 
-            if ($answer1 == "B") { $totalCorrect++; }
-            if ($answer2 == "C") { $totalCorrect++; }
-            if ($answer3 == "A") { $totalCorrect++; }
+			            if ($answer1 == "B") { $totalCorrect++; }
+			            if ($answer2 == "C") { $totalCorrect++; }
+			            if ($answer3 == "A") { $totalCorrect++; }
 						if ($answer4 == "B") { $totalCorrect++; }
 						if ($answer5 == "A") { $totalCorrect++; }
 
