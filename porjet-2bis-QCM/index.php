8ee90745a01f28f707a115ccf3973a6413082ed9
bethek <kreshnikiberdemaj@gmail.com>
2017-06-28 00:21:47 +0200

    Update styling centering

--- porjet-2bis-QCM/index.php
+++ porjet-2bis-QCM/index.php
@@ -3,10 +3,11 @@
 
 <head>
           <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
           <meta http-equiv="X-UA-Compatible" content="ie=edge">
           <link rel="stylesheet" href="css/bootstrap.min.css">
-          <link rel="stylesheet" href="css/style.css">
+          <link rel="stylesheet" href="css/main.css">
+
           <title>My first QCM</title>
 </head>
 
@@ -17,31 +18,32 @@
 
 <div class="container">
     <div class="row">
+        <div class="center-block">
         <div class="col-md-12">
                 <form class="form-inline" action="reponses.php" method="post">
                     <div class="form-group">
-                    <label for="labelemail">Name</label>
-                    <input type="text" class="form-control" name="name" id="name" placeholder="Your name">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail2">Email</label>
-                    <input type="email" class="form-control" name="mail" id="mail" placeholder="youremail@demo.com">
-                </div>
+                        <label for="labelemail">Name</label>
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Your name">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail2">Email</label>
+                        <input type="email" class="form-control" name="mail" id="mail" placeholder="youremail@demo.com">
+                    </div>
 
           <ol>
             <li>
             <h3>Quelle est la distribution linux la plus connue ? </h3>
-            <div>
+            <div class="button_inline">
                 <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
                 <label for="question-1-answers-A">A) Arch Linux </label>
             </div>
 
-              <div>
+              <div class="button_inline">
                 <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
                 <label for="question-1-answers-B">B) Ubuntu</label>
               </div>
 
-              <div>
+              <div class="button_inline">
                 <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
                 <label for="question-1-answers-C">C) Kali linux</label>
               </div>
@@ -52,16 +54,16 @@
             <li>
               <h3>Comment s'appelle la dernière version de Arch Linux au niveau de la sécurité</h3>
 
-              <div>
+              <div class="button_inline">
                 <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
                 <label for="question-2-answers-A">A) Arch Linux</label>
             </div>
-                <div>
+                <div class="button_inline">
                 <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
                 <label for="question-2-answers-B">B) Linux Antergos</label>
               </div>
 
-              <div>
+              <div class="button_inline">
                 <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
                 <label for="question-2-answers-C">C) Black Arch Linux</label>
               </div>
@@ -70,34 +72,34 @@
             <li>
               <h3>Qu'est ce que le bitcoin ? </h3>
 
-                  <div>
+                  <div class="button_inline">
                     <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
                     <label for="question-3-answers-A">A) Une monnaie cryptée</label>
                   </div>
 
-                  <div>
+                  <div class="button_inline">
                     <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
                     <label for="question-3-answers-B">B) Une nouvelle devise</label>
                   </div>
 
-                  <div>
+                  <div class="button_inline">
                     <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
                     <label for="question-3-answers-C">C) Un nouveau software</label>
                   </div>
                   <li>
                     <h3>Que veut dire ETH ? </h3>
 
-                    <div>
+                    <div class="button_inline">
                       <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
                       <label for="question-4-answers-A">A) Ether</label>
                     </div>
 
-                    <div>
+                    <div class="button_inline">
                       <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
                       <label for="question-4-answers-B">B) Ethereum</label>
                     </div>
 
-                    <div>
+                    <div class="button_inline">
                       <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
                       <label for="question-4-answers-C">C) EMS</label>
                     </div>
@@ -105,17 +107,17 @@
                   <li>
                     <h3>Qu'est-ce que Tor? </h3>
 
-                    <div>
+                    <div class="button_inline">
                       <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
                       <label for="question-5-answers-A">A) Navigateur anonyme</label>
                     </div>
 
-                    <div>
+                    <div class="button_inline">
                       <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
                       <label for="question-5-answers-B">B) un Nounours</label>
                     </div>
 
-                    <div>
+                    <div class="button_inline">
                       <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
                       <label for="question-5-answers-C">C) Un nouveau hardware</label>
                     </div>
@@ -125,18 +127,17 @@
 
               </ol>
 
-              <button type="submit" name="envoyer" class="btn btn-default">ENVOYER</button>
-
-          </button>
-
-        </form>
+                <button type="submit" name="envoyer" class="btn btn-default">Envoyer</button>
+                </form>
         </div>
-            <footer>
-                <p>This the footer content AREA of this page</p>
-            </footer>
+
     </div>
   </div>
+
 </div>
+<footer>
+    <p>This the footer content AREA of this page</p>
+</footer>
 </body>
 
 </html>
--- porjet-2bis-QCM/reponses.php
+++ porjet-2bis-QCM/reponses.php
@@ -1,65 +1,50 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-	<meta charset="UTF-8">
-	<title>PHP Quiz</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css" />
-</head>
+					//SANITISATION
+					if(isset($_POST["envoyer"])){
+							$SANI_name =	filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+							$SANI_mail =	filter_var($_POST["mail"], FILTER_SANITIZE_EMAIL);
 
-<body>
+							//print_r ($_POST);
 
-				<?php
-
-				
+					//VALIDATION
+					if(!empty($SANI_name) && !empty($SANI_mail)){
+							//echo "Bonjour";
+					}
+					else{
+							echo "Votre e-mail n'est pas valide";
+						}
+					}
 
 					if(isset($_POST["envoyer"])){
-						$email = $_POST["mail"];
-						$name = $_POST["name"];
+							$email = $_POST["mail"];
+							$name = $_POST["name"];
+							// the message
+							$msg = "Message reçu de Quizz from server";
+							// send email
+							mail("kreshnikiberdemaj@gmail.com","QCM reponses",$msg);
+							//mail("info@bethek.5gbfree.com","QCM reponses",$msg);
+
+
+				            $answer1 = $_POST['question-1-answers'];
+				            $answer2 = $_POST['question-2-answers'];
+				            $answer3 = $_POST['question-3-answers'];
+							$answer4 = $_POST['question-4-answers'];
+							$answer5 = $_POST['question-5-answers'];
 
 
 
-					// the message
-					$msg = "Message reçu de Quizz from server";
+	            			$totalCorrect = 0;
+
+				            if ($answer1 == "B") { $totalCorrect++; }
+				            if ($answer2 == "C") { $totalCorrect++; }
+				            if ($answer3 == "A") { $totalCorrect++; }
+							if ($answer4 == "B") { $totalCorrect++; }
+							if ($answer5 == "A") { $totalCorrect++; }
 
 
+	            			echo "<div id=results>Bonjour " . $name ." vous avez " . $totalCorrect . " / 5</div>";
 
 
-					// send email
-					mail("kreshnikiberdemaj@gmail.com","QCM reponses",$msg);
-					mail("info@bethek.5gbfree.com","QCM reponses",$msg);
-
-
-		            $answer1 = $_POST['question-1-answers'];
-		            $answer2 = $_POST['question-2-answers'];
-		            $answer3 = $_POST['question-3-answers'];
-					$answer4 = $_POST['question-4-answers'];
-					$answer5 = $_POST['question-5-answers'];
-
-
-
-            $totalCorrect = 0;
-
-			            if ($answer1 == "B") { $totalCorrect++; }
-			            if ($answer2 == "C") { $totalCorrect++; }
-			            if ($answer3 == "A") { $totalCorrect++; }
-						if ($answer4 == "B") { $totalCorrect++; }
-						if ($answer5 == "A") { $totalCorrect++; }
-
-
-            echo "<div id=results>Bonjour " . $name ." vous avez " . $totalCorrect . " / 5</div>";
-
-
-						}
-        ?>
-
-
-
-	</div>
-
-
-
-</body>
-
-</html>
+					}
+?>
